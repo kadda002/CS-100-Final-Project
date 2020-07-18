90701d3e27b745abbfbcbdfced3a3cfe87967a15
Zachattack98 <zhill003@ucr.edu>
2020-07-18 01:38:14 -0700

    All headers files in toppings directory

---
+++ toppings/toppings.hpp
@@ -0,0 +1,17 @@
+#ifndef __TOPPINGS_HPP__
+#define __TOPPINGS_HPP__
+
+#include "anchovy.hpp"
+#include "bellpepper.hpp"
+#include "mushroom.hpp"
+#include "pepperoni.hpp"
+#include "chicken.hpp"
+#include "meatball.hpp"
+#include "spinach.hpp"
+#include "olive.hpp"
+#include "tomato.hpp"
+#include "onion.hpp"
+#include "salami.hpp"
+#include "tuna.hpp"
+
+#endif // __TOPPINGS_HPP__

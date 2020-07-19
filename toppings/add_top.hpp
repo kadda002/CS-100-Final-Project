3801ac64d118b8fdcda52ed30c933714d7e9326b
Zachattack98 <zhill003@ucr.edu>
2020-07-19 03:53:41 -0700

    Created function for receiving all character of input for toppings

---
+++ toppings/add_top.hpp
@@ -0,0 +1,25 @@
+#ifndef __ADD_TOP_HPP__
+#define __ADD_TOP_HPP__
+
+#include <iostream>
+#include <cctype>
+
+#include "../pizza.hpp"
+
+Pizza* add_top(char** input, int length) {
+	/*Initializing loop*/
+	Pizza* top;	//Initialize single character in topping name
+     
+	for(int i=0; i < length; i++) {
+		if(isalpha(input[i])) {
+			top = new Order(input[i]);
+		}
+		else {
+			return nullptr;
+		}
+	}
+
+	return top;	//either return character or null based on input
+}
+
+#endif // __ADD_TOP_HPP__

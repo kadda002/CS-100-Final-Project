2bb4ee15e9280b3a5366a448ffce3fb2ac04d247
Zachattack98 <zhill003@ucr.edu>
2020-07-19 04:09:18 -0700

    Asking user for topping input

--- main.cpp
+++ main.cpp
@@ -4,7 +4,45 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+	Pizza* pizza = new Pizza();
+	string choice_topping;
+	bool add_more = true;
 
-return 0;
+	while(valid_topping) {
+		cout << "Enter any (more) toppings from the list you wish to add to your pizza, or enter N if not: " << endl;
+		// cin >> choice_topping;
+		switch(choice_topping) {
+			case Anchovy:
+				pizza = new Anchovy(pizza); break;
+			case Bellpepper:
+                                pizza = new Bellpepper(pizza); break;
+			case Chicken:
+                                pizza = new Chicken(pizza); break;
+			case Meatball:
+                                pizza = new Anchovy(pizza); break;
+			case Mushroom:
+                                pizza = new Anchovy(pizza); break;
+			case Olive:
+                                pizza = new Anchovy(pizza); break;
+			case Onion:
+                                pizza = new Anchovy(pizza); break;
+			case Pepperoni:
+                                pizza = new Anchovy(pizza); break;
+			case Salami:
+                                pizza = new Anchovy(pizza); break;
+			case Spinach:
+                                pizza = new Anchovy(pizza); break;
+			case Tomato:
+                                pizza = new Anchovy(pizza); break;
+			case Tuna:
+                                pizza = new Anchovy(pizza); break;
+			case N:
+				add_more = false; break;
+			default:
+				cout << "Invalid Input!!" <<endl;
+		}
+	}
+
+	return 0;
 }

a6743db8374734243cb5f7631ddf1eb71e134c34
Zachattack98 <zhill003@ucr.edu>
2020-07-21 18:07:53 -0700

    More test cases added

--- toppings/toppings_tests.hpp
+++ toppings/toppings_tests.hpp
@@ -2,10 +2,11 @@
 #define __TOPPINGS_TESTS_HPP__
 
 #include "gtest/gtest.h"
+#include <iostream>
 #include <string>
-#include <vector>
 
-#include "../pizza.hpp"
+#include "../pizza_decorator.hpp"
+#include "../toppings_list.hpp"
 #include "toppings_decorator.hpp"
 #include "toppings.hpp"
 
@@ -14,20 +15,41 @@
 //***************************
 
 TEST(ToppingsTest, DecorateTest1) {
-	Pizza* topp = new ToppingsDecorator();
+	Pizza_Decorator* topp = new ToppingsDecorator();
 	Anchovy* anch = new Anchovy(topp);
 
 	EXPECT_EQ(anch->get_topping(), " Anchovy");
 	EXPECT_EQ(anch->get_cost(), 4.10);
-	
-	BellPepper* bell = new BellPepper(topp);
+}
+
+TEST(ToppingsTest, DecorateTest2) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Anchovy* anch = new Anchovy(topp);
+
+        EXPECT_EQ(anch->get_topping(), " Anchovy");
+        EXPECT_EQ(anch->get_cost(), 4.10);
+
+        BellPepper* bell = new BellPepper(topp);
 
         EXPECT_EQ(bell->get_topping(), " YellowBellPepper");
         EXPECT_EQ(bell->get_cost(), 1.55);
 }
 
-TEST(ToppingsTest, DecorateTest2) {
-        Pizza* topp = new ToppingsDecorator();
+TEST(ToppingsTest, DecorateTest3) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Chicken* anch = new Chicken(topp);
+
+        EXPECT_EQ(anch->get_topping(), " Chicken");
+        EXPECT_EQ(anch->get_cost(), 2.35);
+
+        BellPepper* bell = new BellPepper(topp);
+
+        EXPECT_EQ(bell->get_topping(), " YellowBellPepper");
+        EXPECT_EQ(bell->get_cost(), 1.55);
+}
+
+TEST(ToppingsTest, DecorateTest4) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
         Chicken* chicken = new Chicken(topp);
 
         EXPECT_EQ(chicken->get_topping(), " Chicken");
@@ -39,8 +61,21 @@ TEST(ToppingsTest, DecorateTest2) {
         EXPECT_EQ(mush->get_cost(), 1.55);
 }
 
-TEST(ToppingsTest, DecorateTest3) {
-        Pizza* topp = new ToppingsDecorator();
+TEST(ToppingsTest, DecorateTest5) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Chicken* chicken = new Chicken(topp);
+
+        EXPECT_EQ(chicken->get_topping(), " Chicken");
+        EXPECT_EQ(chicken->get_cost(), 2.35);
+
+        Meatball* mb = new Meatball(topp);
+        Mushroom* mush = new Mushroom(mb);
+        EXPECT_EQ(mb->get_topping(), " Meatball");
+        EXPECT_EQ(mb->get_cost(), 2.35);
+}
+
+TEST(ToppingsTest, DecorateTest6) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
         Pepperoni* pepp = new Pepperoni(topp);
 	Spinach* spinach = new Spinach(pepp);
 
@@ -54,8 +89,53 @@ TEST(ToppingsTest, DecorateTest3) {
         EXPECT_EQ(onion->get_cost(), 1.55);
 }
 
-TEST(ToppingsTest, DecorateTest4) {
-        Pizza* topp = new ToppingsDecorator();
+TEST(ToppingsTest, DecorateTest7) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Pepperoni* pepp = new Pepperoni(topp);
+        Spinach* spinach = new Spinach(pepp);
+
+        EXPECT_EQ(spinach->get_topping(), " Spinach");
+        EXPECT_EQ(spinach->get_cost(), 1.55);
+
+        Olive* olive = new Olive(pepp);
+        Onion* onion = new Onion(onion);
+
+        EXPECT_EQ(olive->get_topping(), " Olive");
+        EXPECT_EQ(olive->get_cost(), 1.55);
+}
+
+TEST(ToppingsTest, DecorateTest8) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Pepperoni* pepp = new Pepperoni(topp);
+        Spinach* spinach = new Spinach(pepp);
+
+        EXPECT_EQ(pepp->get_topping(), " Pepperoni");
+        EXPECT_EQ(pepp->get_cost(), 2.35);
+
+        Olive* olive = new Olive(pepp);
+        Onion* onion = new Onion(onion);
+
+        EXPECT_EQ(onion->get_topping(), " Onion");
+        EXPECT_EQ(onion->get_cost(), 1.55);
+}
+
+TEST(ToppingsTest, DecorateTest9) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Pepperoni* pepp = new Pepperoni(topp);
+        Spinach* spinach = new Spinach(pepp);
+
+        EXPECT_EQ(pepp->get_topping(), " Pepperoni");
+        EXPECT_EQ(pepp->get_cost(), 2.35);
+
+        Olive* olive = new Olive(pepp);
+        Onion* onion = new Onion(onion);
+
+        EXPECT_EQ(olive->get_topping(), " Olive");
+        EXPECT_EQ(olive->get_cost(), 1.55);
+}
+
+TEST(ToppingsTest, DecorateTest10) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
         Pepperoni* pepp = new Pepperoni(topp);
 	Salami* sal = new Salami(pepp);
 	Tomato* tom = new Tomato(sal);
@@ -66,69 +146,62 @@ TEST(ToppingsTest, DecorateTest4) {
         EXPECT_EQ(olive->get_cost(), 1.55);
 }
 
-//***************************
-// Input Tests
-//***************************
+TEST(ToppingsTest, DecorateTest11) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Pepperoni* pepp = new Pepperoni(topp);
+        Salami* sal = new Salami(pepp);
+        Tomato* tom = new Tomato(sal);
+        Tuna* tuna = new Tuna(tom);
+        Olive* olive = new Olive(tuna);
 
-TEST(ToppingsTest, ValidInputTest) {
-	std::vector<std::string> message = {"M", "e", "a", "t", "b", "a", "l", "l"};
-	std::vector<char*> argv;
-	for (const auto& arg : message)
-		argv.push_back((char*)arg.data());
-	argv.push_back(nullptr);
-
-	Pizza *topping = add_top(argv.data(), argv.size()-1);
-	
-	EXPECT_EQ(topping->get_topping(), "Meatball");
-	EXPECT_EQ(topping->get_cost(), 2.35);
-	
+        EXPECT_EQ(tuna->get_topping(), " Tuna");
+        EXPECT_EQ(tuna->get_cost(), 4.10);
 }
 
-TEST(ToppingsTest, InvalidInputTest) {
-	//Validation #1
-        std::vector<std::string> message1 = {"8"};
-        std::vector<char*> argv1;
-        for (const auto& arg1 : message1)
-                argv1.push_back((char*)arg1.data());
-        argv1.push_back(nullptr);
+TEST(ToppingsTest, DecorateTest12) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Pepperoni* pepp = new Pepperoni(topp);
+        Salami* sal = new Salami(pepp);
+        Tomato* tom = new Tomato(sal);
+        Tuna* tuna = new Tuna(tom);
+        Olive* olive = new Olive(tuna);
 
-        Pizza *topping = add_top(argv1.data(), argv1.size()-1);
+        EXPECT_EQ(tom->get_topping(), " DriedTomato");
+        EXPECT_EQ(tom->get_cost(), 1.55);
+}
 
-        EXPECT_EQ(topping, nullptr);
+TEST(ToppingsTest, DecorateTest13) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Pepperoni* pepp = new Pepperoni(topp);
+        Salami* sal = new Salami(pepp);
+        Tomato* tom = new Tomato(sal);
+        Tuna* tuna = new Tuna(tom);
+        Olive* olive = new Olive(tuna);
 
-	//Validation #2
-	std::vector<std::string> message2 = {"R", "5", "wrong"};
-        std::vector<char*> argv2;
-        for (const auto& arg2 : message2)
-                argv2.push_back((char*)arg2.data());
-        argv2.push_back(nullptr);
+        EXPECT_EQ(sal->get_topping(), " Salami");
+        EXPECT_EQ(sal->get_cost(), 2.35);
+}
 
-        topping = add_top(argv2.data(), argv2.size()-1);
+TEST(ToppingsTest, DecorateTest14) {
+        Pizza_Decorator* topp = new ToppingsDecorator();
+        Pepperoni* pepp = new Pepperoni(topp);
+        Salami* sal = new Salami(pepp);
+        Tomato* tom = new Tomato(sal);
+        Tuna* tuna = new Tuna(tom);
+        Olive* olive = new Olive(tuna);
 
-        EXPECT_EQ(topping, nullptr);
+        EXPECT_EQ(pepp->get_topping(), " Pepperoni");
+        EXPECT_EQ(pepp->get_cost(), 2.35);
+}
 
-	//Validation #3
-	std::vector<std::string> message3 = {"M", "E", "A", "T", "B", "A", "L", "L"};
-        std::vector<char*> argv3;
-        for (const auto& arg3 : message3)
-                argv3.push_back((char*)arg3.data());
-        argv3.push_back(nullptr);
+//***************************
+// Topping Cost Tests
+//***************************
 
-        topping = add_top(argv3.data(), argv3.size()-1);
+/*TEST(ToppingsTest, ListTest) {
+	Toppings_List* pizza;
 
-        EXPECT_EQ(topping, nullptr);	
-
-	//Validation #4
-	std::vector<std::string> message4 = {"M", "e", "a", "t", "b", "#", "l", "l"};
-        std::vector<char*> argv4;
-        for (const auto& arg4 : message4)
-                argv4.push_back((char*)arg4.data());
-        argv4.push_back(nullptr);
-
-        topping = add_top(argv4.data(), argv4.size()-1);
-
-        EXPECT_EQ(topping, nullptr);
-
-} 	
-
+	pizza->topping_details();
+}*/
+	
 #endif // __TOPPINGS_TESTS_HPP

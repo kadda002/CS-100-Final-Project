c61f11503aa7fbeed32ddfb1a1cbc245a59df5b1
kadda002 <kadda002@ucr.edu>
2020-07-23 00:26:40 -0700

    Modified the factory output so that everything has a base class of pizza

---
+++ base_pizza.hpp
@@ -0,0 +1,21 @@
+#ifndef __BASE_PIZZA_HPP__
+#define __BASE_PIZZA_HPP__
+
+#include <string>
+using namespace std;
+#include "Factory_comp/Crust.hpp"
+#include "Factory_comp/Cheese.hpp"
+#include "Factory_comp/Sauce.hpp"
+#include "pizza.hpp"
+class Base_Pizza : public Pizza{
+        public:
+                //Constructors
+                Base_Pizza(string t){ type = t;};
+
+                //Functions
+                string get_topping() { return "no topping"; }
+                double get_cost() {return 0; }
+                
+};
+
+#endif  // __BASE_PIZZA_HPP__
